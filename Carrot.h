a7521b9c59819e95236815687ce536c36cf8d8e0
oknik <152298148+oknik@users.noreply.github.com>
2023-12-26 17:50:02 +0800

    Add files via upload

---
+++ Carrot.h
@@ -0,0 +1,27 @@
+#ifndef __CARROT_H__
+#define __CARROT_H__
+
+#include "cocos2d.h"
+
+class Carrot : public cocos2d::Sprite {
+public:
+    int hp; 
+    cocos2d::Label* HPLabel;
+
+    virtual bool init();
+    static Carrot* createCarrot();
+    static cocos2d::Sprite* createHP();
+    void updateCarrotSprite(); 
+    cocos2d::Label* createHPLabel();
+    void updateHPLabel();
+
+    CREATE_FUNC(Carrot);
+
+    Carrot() : hp(0), HPLabel(nullptr) {}
+
+    virtual ~Carrot() {
+        CC_SAFE_RELEASE_NULL(HPLabel);
+    }
+};
+
+#endif // __CARROT_H__
---
+++ Data.h
@@ -0,0 +1,6 @@
+#ifndef __DATA_H__
+#define __DATA_H__
+
+extern bool isFirstMapWon;
+
+#endif // __DATA_H__
